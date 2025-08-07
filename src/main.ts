2f5e220a7ab1357c9d58e62f7e33840f1bb98cf7
Vikash Verma <vikashv.2011@gmail.com>
2025-08-07 17:36:04 +0530

    updatd swagger tag

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,6 @@ async function bootstrap() {
     .setTitle('NestJS API')
     .setDescription('The NestJS API description')
     .setVersion('1.0')
-    .addTag('api')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
