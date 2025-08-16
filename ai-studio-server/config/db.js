dbc4d3a86a9bed702b46afaaf9d4e53b55cb2bad
Vikash Verma <vikashv.2011@gmail.com>
2025-08-16 10:47:17 +0530

    updated code

--- ai-studio-server/config/db.js
+++ ai-studio-server/config/db.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      // useCreateIndex: true, // Deprecated in Mongoose 6.0
-      // useFindAndModify: false // Deprecated in Mongoose 6.0
+      maxPoolSize: 10,
+      minPoolSize: 5,
+      socketTimeoutMS: 45000,
     });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+
+    console.log(`MongoDB Connected: ${conn.connection.host} - "${conn.connection.db.databaseName}"`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1); // Exit process with failure
